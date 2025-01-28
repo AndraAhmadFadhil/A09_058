01314c43ae277eef2b2db15cfb36f7b81f1a90a9
Ann <andra.ahmad.ft22@mail.umy.ac.id>
2025-01-28 22:30:39 +0700

    memperbarui destinasi navigasi untuk tabel lainnya

--- app/src/main/java/com/example/uaspam/ui/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/uaspam/ui/navigation/DestinasiNavigasi.kt
@@ -1,5 +1,7 @@
 package com.example.uaspam.ui.navigation
 
+import com.example.uaspam.ui.navigation.DestinasiDetailPenayangan.ID_PENAYANGAN
+
 
 interface DestinasiNavigasi {
     val route: String
@@ -34,4 +36,81 @@ object DestinasiDetailFilm : DestinasiNavigasi{
     override val titleRes = "Manajemen Detail Film"
     const val ID_FILM = "id_film"
     val routeWithArgs = "$route/{$ID_FILM}"
+}
+
+//STUDIO
+object DestinasiHomeStudio : DestinasiNavigasi{
+    override val route = "Home Studio"
+    override val titleRes = "Manajemen Home Studio"
+}
+
+object DestinasiInsertStudio : DestinasiNavigasi{
+    override val route = "Insert Studio"
+    override val titleRes = "Manajemen Insert Studio"
+}
+
+object DestinasiUpdateStudio : DestinasiNavigasi{
+    override val route = "Update Studio"
+    override val titleRes = "Manajemen Update Studio"
+    const val ID_STUDIO = "id_studio"
+    val routesWithArg = "${route}/{$ID_STUDIO}"
+}
+
+object DestinasiDetailStudio : DestinasiNavigasi{
+    override val route = "Detail Studio"
+    override val titleRes = "Manajemen Detail Studio"
+    const val ID_STUDIO = "id_studio"
+    val routeWithArgs = "$route/{$ID_STUDIO}"
+}
+
+//PENAYANGAN
+object DestinasiHomePenayangan : DestinasiNavigasi{
+    override val route = "Home Penayangan"
+    override val titleRes = "Manajemen Home Penayangan"
+}
+
+object DestinasiInsertPenayangan : DestinasiNavigasi{
+    override val route = "Insert Penayangan"
+    override val titleRes = "Manajemen Insert Penayangan"
+}
+
+object DestinasiUpdatePenayangan : DestinasiNavigasi{
+    override val route = "Update Penayangan"
+    override val titleRes = "Manajemen Update Penayangan"
+    const val ID_PENAYANGAN = "id_penayangan"
+    val routesWithArg = "${route}/{$ID_PENAYANGAN}"
+}
+
+object DestinasiDetailPenayangan : DestinasiNavigasi{
+    override val route = "Detail Penayangan"
+    override val titleRes = "Manajemen Detail Penayangan"
+    const val ID_PENAYANGAN = "id_penayangan"
+    val routeWithArgs = "$route/{$ID_PENAYANGAN}"
+}
+
+//TIKET
+object DestinasiHomeTiket : DestinasiNavigasi{
+    override val route = "Home Tiket"
+    override val titleRes = "Manajemen Home Tiket"
+}
+
+object DestinasiInsertTiket : DestinasiNavigasi{
+    override val route = "Insert Tiket"
+    override val titleRes = "Manajemen Insert Tiket"
+    val routeWithArgs = "$route/{$ID_PENAYANGAN}"
+
+}
+
+object DestinasiUpdateTiket : DestinasiNavigasi{
+    override val route = "Update Tiket"
+    override val titleRes = "Manajemen Update Tiket"
+    const val ID_TIKET = "id_tiket"
+    val routesWithArg = "${route}/{$ID_TIKET}"
+}
+
+object DestinasiDetailTiket : DestinasiNavigasi{
+    override val route = "Detail Tiket"
+    override val titleRes = "Manajemen Detail Tiket"
+    const val ID_TIKET = "id_tiket"
+    val routeWithArgs = "$route/{$ID_TIKET}"
 }
